169135b4a52954386b64a166ec3c989ffcca15a8
Saeid Nejat <nedjat2021@gmail.com>
2026-01-22 16:10:43 +0200

    Fix image paths and filenames

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,10 +1,10 @@
 import "./Team.css";
 
 const teamImages = [
-  "/images/portfolio-1.png",
-  "/images/portfolio-2.png",
-  "/images/portfolio-3.png",
-  "/images/portfolio-1.png",
+  "/images/portfolio-1.jpg",
+  "/images/portfolio-2.jpg",
+  "/images/portfolio-3.jpg",
+  "/images/portfolio-4.jpg",
 ];
 
 function Team() {
@@ -14,7 +14,7 @@ function Team() {
 
       <div className="team-grid">
         {teamImages.map((img, index) => (
-          <img key={index} src={img} alt="Team member" />
+          <img key={index} src={img} alt={`Team member ${index + 1}`} />
         ))}
       </div>
     </section>
